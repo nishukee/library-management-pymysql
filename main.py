6981bb1a84debd96b7823a4276a9cf9282a4f444
nishukee <nishchaymnayak@gmail.com>
2021-01-28 22:30:35 +0530

    Updated from old tk widgets to new ttk widgets except Menu

--- main.py
+++ main.py
@@ -11,36 +11,47 @@ class Book_Register:
         self.breg.title('Book Register')
         self.breg.resizable(False, False)
         #Add Book widgets
-        self.add_book_frame = tk.Frame(self.breg, bg='#7a3b0b')
-        self.book_name_lbl = tk.Label(self.add_book_frame, text="Book Name", bg='#7a3b0b', font=('FreeSans',14))
-        self.book_author_lbl = tk.Label(self.add_book_frame, text="Author", bg='#7a3b0b', font=('FreeSans',14))
-        self.book_isbn_lbl = tk.Label(self.add_book_frame, text="ISBN", bg='#7a3b0b', font=('FreeSans',14))
-        self.book_price_lbl = tk.Label(self.add_book_frame, text="Price", bg='#7a3b0b', font=('FreeSans',14))
+        self.widget_styles()
+        self.add_book_frame = ttk.Frame(self.breg)
+        self.book_name_lbl = ttk.Label(self.add_book_frame, text="Book Name")
+        self.book_author_lbl = ttk.Label(self.add_book_frame, text="Author")
+        self.book_isbn_lbl = ttk.Label(self.add_book_frame, text="ISBN")
+        self.book_price_lbl = ttk.Label(self.add_book_frame, text="Price")
         self.book_name = ""
-        self.book_name_entry = tk.Entry(self.add_book_frame, bg='#4a2910')
+        self.book_name_entry = ttk.Entry(self.add_book_frame)
         self.author = ""
-        self.book_author_entry = tk.Entry(self.add_book_frame, bg='#4a2910')
+        self.book_author_entry = ttk.Entry(self.add_book_frame)
         self.isbn = ""
-        self.book_isbn_entry = tk.Entry(self.add_book_frame, bg='#4a2910')
+        self.book_isbn_entry = ttk.Entry(self.add_book_frame)
         self.price = ""
-        self.book_price_entry = tk.Entry(self.add_book_frame, bg='#4a2910')
-        self.submit_btn = tk.Button(self.add_book_frame, text="Submit",command=self.book_to_database, bg='#512202', activebackground='#4a2910')
-        self.quit_breg_btn = tk.Button(self.add_book_frame, text="Quit", command=self.breg.destroy, bg='#512202', activebackground='red')
+        self.book_price_entry = ttk.Entry(self.add_book_frame)
+        self.submit_btn = ttk.Button(self.add_book_frame, text="Submit",command=self.book_to_database)
+        self.s.configure('quitbtn.TButton', background="#894b10", foreground="white")
+        self.s.map('quitbtn.TButton',background=[('active','#d10c0c'),('pressed','red')])
+        self.quit_breg_btn = ttk.Button(self.add_book_frame, text="Quit", command=self.breg.destroy, style='quitbtn.TButton')
 
     #Methods for regisering book to database
+    def widget_styles(self):
+        self.s = ttk.Style(self.breg)
+        self.s.theme_use('clam')
+        self.s.configure('TFrame', background='#70340c', foreground='white', fieldbackground='#70340c')
+        self.s.configure('TLabel',foreground='snow',background='#70340c', font=('FreeSans',14))
+        self.s.configure('TEntry', background='white', foreground='black')
+        self.s.configure('TButton', background="#894b10", foreground="white")
+        self.s.map('TButton',background=[('active','#a5570e'),('pressed','#70340c')])
 
     def add_book_window(self):
         self.declare_book_reg_widgets()
         self.breg.lift()
-        self.add_book_frame.grid(row=0, column=0, sticky='nsew')
-        self.book_name_lbl.grid(row=2, column=1, ipadx=3)
-        self.book_author_lbl.grid(row=3,column=1, ipadx=3)
-        self.book_isbn_lbl.grid(row=4,column=1, ipadx=3)
-        self.book_price_lbl.grid(row=5,column=1, ipadx=3)
-        self.book_name_entry.grid(row=2, column=3, ipadx=40)
-        self.book_author_entry.grid(row=3, column=3, ipadx=40)
-        self.book_isbn_entry.grid(row=4, column=3, ipadx=40)
-        self.book_price_entry.grid(row=5, column=3, ipadx=40)
+        self.add_book_frame.grid(row=0, column=0, sticky='nsew', ipadx=5, ipady=5)
+        self.book_name_lbl.grid(row=2, column=1, ipadx=3, padx=5)
+        self.book_author_lbl.grid(row=3,column=1, ipadx=3, padx=5)
+        self.book_isbn_lbl.grid(row=4,column=1, ipadx=3, padx=5)
+        self.book_price_lbl.grid(row=5,column=1, ipadx=3, padx=5)
+        self.book_name_entry.grid(row=2, column=3, ipadx=40, pady=10)
+        self.book_author_entry.grid(row=3, column=3, ipadx=40, pady=10)
+        self.book_isbn_entry.grid(row=4, column=3, ipadx=40, pady=10)
+        self.book_price_entry.grid(row=5, column=3, ipadx=40, pady=10)
         self.submit_btn.grid(row=7, column=2, sticky='s')
         self.quit_breg_btn.grid(row=7, column=3, sticky='s')
 
@@ -74,17 +85,27 @@ class Book_View:
         self.bview.title('Books Table')
         self.bview.resizable(False, False)
          #View Book widgets
-        self.view_book_frame = tk.Frame(self.bview)
+        self.widget_styles()
+        self.view_book_frame = ttk.Frame(self.bview)
         self.cols = ('Book ID','Book Name','Author','ISBN','Price','Status')
         self.list_books = ttk.Treeview(self.view_book_frame, columns=self.cols, show='headings', selectmode='browse')
-        self.verscrlbar = ttk.Scrollbar(self.view_book_frame, orient=tk.VERTICAL,command=self.list_books.yview)
-        self.quit_view_btn = tk.Button(self.view_book_frame, text="Quit View", command=self.bview.destroy)
+        self.verscrlbar = ttk.Scrollbar(self.view_book_frame, orient=tk.VERTICAL, command=self.list_books.yview)
+        self.quit_view_btn = ttk.Button(self.view_book_frame, text="Quit View", command=self.bview.destroy)
         for self.col in self.cols:
             self.list_books.heading(self.col, text=self.col)
         self.book_details = []
         self.index = self.iid = 0
     
         #Methods for Viewing Book List
+    def widget_styles(self):
+        self.s = ttk.Style(self.bview)
+        self.s.theme_use('clam')
+        self.s.configure('TFrame', background='silver', foreground='black', fieldbackground='silver')
+        self.s.configure('Treeview', background='silver', foreground='black', fieldbackground='silver')
+        self.s.configure('TScrollbar',background='silver', foreground='black')
+        self.s.map('TScollbar',background=[('pressed','white')])
+        self.s.configure('TButton', background="gray", foreground="black")
+        self.s.map('TButton',background=[('active','red'),('pressed','red')])
 
     def view_book_window(self):
         self.declare_view_widgets()
@@ -99,7 +120,7 @@ class Book_View:
             self.index = self.iid = self.index + 1
         self.verscrlbar.grid(row=1,column=0, sticky='ns')
         self.list_books.configure(yscrollcommand = self.verscrlbar.set)
-        self.quit_view_btn.grid(row=4,column=3)
+        self.quit_view_btn.grid(row=4,column=3,columnspan=2, padx=20, pady=5)
 
 class Book_Delete:
     def declare_book_delete_widgets(self):
@@ -107,48 +128,61 @@ class Book_Delete:
         self.bdel.title('Delete Books')
         self.bdel.resizable(False, False)
         self.retrieve_books()
-        self.book_delete_frame = tk.Frame(self.bdel)
+        self.widget_styles()
+        self.book_delete_frame = ttk.Frame(self.bdel)
         self.book_id_str = tk.StringVar()
         self.combo_box = ttk.Combobox(self.book_delete_frame, width=27, textvariable = self.book_id_str, values=self.bookids)
         self.combo_box.set('Pick Book ID')
+        self.combo_box.bind("<<ComboboxSelected>>",self.check_book)
         self.book_name_str = tk.StringVar()
-        self.del_book_entry = tk.Entry(self.book_delete_frame, textvariable=self.book_name_str)
+        self.del_book_entry = ttk.Entry(self.book_delete_frame, textvariable=self.book_name_str)
         self.del_book_entry.insert(0, 'Book Name')
-        self.chk_book_btn = tk.Button(self.book_delete_frame, text="Check Book Name", command=self.check_book)
-        self.del_btn = tk.Button(self.book_delete_frame, text="Delete", command=self.book_delete)
-        self.quit_del_btn = tk.Button(self.book_delete_frame, text="Quit", command=self.bdel.destroy)
+        self.del_btn = ttk.Button(self.book_delete_frame, text="Delete", command=self.book_delete)
+        self.s.configure('quitbtn.TButton', background="#894b10", foreground="white")
+        self.s.map('quitbtn.TButton',background=[('active','#d10c0c'),('pressed','red')])
+        self.quit_del_btn = ttk.Button(self.book_delete_frame, text="Quit", command=self.bdel.destroy, style='quitbtn.TButton')
     
+    def widget_styles(self):
+        self.s = ttk.Style(self.bdel)
+        self.s.theme_use('clam')
+        self.s.configure('TFrame', background='#70340c', foreground='white', fieldbackground='#70340c')
+        self.s.configure('TCombobox', background="#894b10", foreground='white')
+        self.s.map('TCombobox',background=[('active','#a5570e'),('pressed','#70340c')])
+        self.s.configure('TEntry', background='white', foreground='black')
+        self.s.configure('TButton', background="#894b10", foreground="white")
+        self.s.map('TButton',background=[('active','#a5570e'),('pressed','#70340c')])
+
     def retrieve_books(self):
         self.bookids, self.bookdict = sql3.get_del_book_details()
     
     def delete_book_window(self):
         self.declare_book_delete_widgets()
         self.bdel.lift()
-        self.book_delete_frame.grid(row=0, column=0)
-        self.combo_box.grid(row=2, column=1)
-        self.del_book_entry.grid(row=2, column=3)
-        self.chk_book_btn.grid(row=4, column=2)
-        self.del_btn.grid(row=4, column=3)
-        self.quit_del_btn.grid(row=4, column=4)
-
-    def check_book(self):
-        if self.combo_box.get()=='Pick Book ID':
-            messagebox.showwarning('Value not selected','Select Book ID')
-        else :
+        self.book_delete_frame.grid(row=0, column=0, ipadx=10)
+        self.combo_box.grid(row=2, column=0, padx=10, pady=20)
+        self.del_book_entry.grid(row=2, column=1, pady=20)
+        self.del_btn.grid(row=4, column=0, ipadx=10)
+        self.quit_del_btn.grid(row=4, column=1)
+            
+    def check_book(self, event):
+        if self.combo_box.get()!='Pick Book ID':
             self.book_id_str = self.combo_box.get()
             self.book_name_str = self.bookdict[self.book_id_str]
             self.del_book_entry.delete(0, tk.END)
             self.del_book_entry.insert(0, self.book_name_str)
-        
+
     def book_delete(self):
-        messagebox.showinfo("Connecting to Database","Deleting Book")
-        if sql3.delete_book(self.book_id_str)==True:
-            messagebox.showinfo('Connection Succesful','Book Deleted')
-            del self.bookdict[self.book_id_str]
-            self.bdel.destroy()
-            self.delete_book_window()
+        if self.combo_box.get()=='Pick Book ID':
+            messagebox.showwarning('Value not selected','Select Book ID')
         else:
-            messagebox.showerror('Connection Unsuccesful','Book not deleted')
+            messagebox.showinfo("Connecting to Database","Deleting Book")
+            if sql3.delete_book(self.book_id_str)==True:
+                messagebox.showinfo('Connection Succesful','Book Deleted')
+                del self.bookdict[self.book_id_str]
+            else:
+                messagebox.showerror('Connection Unsuccesful','Book not deleted')
+        self.bdel.destroy()
+        self.delete_book_window()
 
 class MainWindow:
 
@@ -156,32 +190,36 @@ class MainWindow:
         self.parent = parent
         self.parent.title('Library')
         self.parent.resizable(False, False)
+        self.s = ttk.Style(self.parent)
+        self.s.theme_use('clam')
+        self.s.configure('TFrame', background='#70340c', foreground='#70340c', fieldbackground='#70340c')
+        self.s.configure('TLabel', background='#70340c', foreground='#70340c', fieldbackground='#70340c')
         self.bg_image = Image.open('Book_shelf.png')
         self.bg_image = self.bg_image.resize((600,300), Image.ANTIALIAS)
         self.img = ImageTk.PhotoImage(self.bg_image)
-        self.main_frame = tk.Frame(parent)
-        self.bg_img_lbl = tk.Label(image=self.img)
-        self.menuBar = tk.Menu(self.parent, bg='#75421d', activebackground='#7a3b0b')
+        self.main_frame = ttk.Frame(parent)
+        self.bg_img_lbl = ttk.Label(image=self.img)
+        self.menuBar = tk.Menu(self.parent, bg='#5e220a', activebackground='#7a3b0b')
         self.parent.config(menu=self.menuBar)
 
-        self.bookOp_menu = tk.Menu(self.menuBar, bg='#4a2910', activebackground='#663109')
+        self.bookOp_menu = tk.Menu(self.menuBar, bg='#70340c', activebackground='#a5570e', tearoff=0)
         self.menuBar.add_cascade(label="Books", menu=self.bookOp_menu)
         self.bookOp_menu.add_command(label="Register Books to Database", command=self.add_book)
         self.bookOp_menu.add_command(label="View Book List", command=self.view_book)
         self.bookOp_menu.add_command(label="Delete Books from Database", command=self.delete_book)
         self.bookOp_menu.add_command(label="Book Status")
 
-        self.bookIsR_menu = tk.Menu(self.menuBar, bg='#4a2910', activebackground='#663109')
+        self.bookIsR_menu = tk.Menu(self.menuBar, bg='#70340c', activebackground='#a5570e', tearoff=0)
         self.menuBar.add_cascade(label="Book Issue/Return", menu=self.bookIsR_menu)
         self.bookIsR_menu.add_command(label="Issue Books")
         self.bookIsR_menu.add_command(label="Return Books")
 
-        self.bookMembers_menu = tk.Menu(self.menuBar, bg='#4a2910', activebackground='#663109')
+        self.bookMembers_menu = tk.Menu(self.menuBar, bg='#70340c', activebackground='#a5570e', tearoff=0)
         self.menuBar.add_cascade(label="Library Members", menu=self.bookMembers_menu)
         self.bookMembers_menu.add_command(label="Register New Member")
         self.bookMembers_menu.add_command(label="Delete Registered Member")
 
-        self.quit_btn = tk.Menu(self.menuBar, bg='#4a2910', activebackground='red')
+        self.quit_btn = tk.Menu(self.menuBar, bg='#70340c', activebackground='red', tearoff=0)
         self.menuBar.add_cascade(label="Quit", menu=self.quit_btn)
         self.quit_btn.add_command(label="Exit Program", command=quit)
         self.book_reg = Book_Register()
